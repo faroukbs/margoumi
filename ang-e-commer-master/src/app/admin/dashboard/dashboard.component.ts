98aabea3ce889b02eb0539c9d8ed940648b0edc2
faroukbs <farouk.boussaid@esprit.tn>
2022-07-28 12:52:43 +0100

    fix

--- ang-e-commer-master/src/app/admin/dashboard/dashboard.component.ts
+++ ang-e-commer-master/src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
     '../../../assets/admin/demo/demo.css',
     '../../../assets/admin/css/bootstrap.min.css',
   ],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class DashboardComponent implements OnInit {
 
